2e0b75962fc6931f39e7cc3a004ca9de2dfa3043
Kamilia Nurmala <nurmalakamilia@gmail.com>
2025-04-18 18:11:57 +0700

    Inisialisasi project dan setup koneksi ke database SQL Server

---
+++ BiroWisataDatabase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+
+namespace BiroWisataConsole
+{
+    class Program
+    {
+        static string connectionString =
+            "Data Source=KAMILIA\\KAMILIANURMALA;Initial Catalog=DbProgramSiswa;Integrated Security=True;";
+    }
+}
